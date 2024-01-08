507d4423fc15338714015d8e81b94148314b3436
CarltonNcube <carltonncube@outlook.com>
2024-01-08 09:32:41 -0800

    endpoint (route)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""app.py to connect to API"""
-import os
-from models import storage
-from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-from flask_cors import CORS
-
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
-    storage.close()
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
-=======
 """Main application module for the AirBnB API"""
 from flask import Flask, make_response, jsonify
 from models import storage
@@ -53,4 +25,3 @@ if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
->>>>>>> 36505c1647ff288e6c8ad4385e1e78c06ff9b35e
