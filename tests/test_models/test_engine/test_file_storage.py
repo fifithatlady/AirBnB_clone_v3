4088d3d2632d64c5ce05f513d3f6ca7c8f91526e
CarltonNcube <carltonncube@outlook.com>
2024-01-08 09:04:01 -0800

    Update DBStorage and FileStorage unittests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -30,14 +30,14 @@ class TestFileStorageDocs(unittest.TestCase):
         """Set up for the doc tests"""
         cls.fs_f = inspect.getmembers(FileStorage, inspect.isfunction)
 
-   def test_get(self):
+    def test_get(self):
         """Test the get method"""
         storage = FileStorage()
-        state = State(name="California")
-        storage.new(state)
-        storage.save()
-        retrieved_state = storage.get(State, state.id)
-        self.assertEqual(state, retrieved_state)
+        self.assertIs(storage.get("User", "blah"), None)
+        self.assertIs(storage.get("blah", "blah"), None)
+        new_user = User()
+        new_user.save()
+        self.assertIs(storage.get("User", new_user.id), new_user)
 
     def test_count(self):
         """Test the count method"""
@@ -140,7 +140,7 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
                      "not testing file storage")
     def test_get(self):
-        """Test that the get method properly retrievs objects"""
+        """Test that the get method properly retrieves objects"""
         storage = FileStorage()
         self.assertIs(storage.get("User", "blah"), None)
         self.assertIs(storage.get("blah", "blah"), None)
@@ -160,3 +160,4 @@ class TestFileStorage(unittest.TestCase):
         new_state.save()
         self.assertEqual(storage.count(), initial_length + 1)
         self.assertEqual(storage.count("State"), state_len + 1)
+
