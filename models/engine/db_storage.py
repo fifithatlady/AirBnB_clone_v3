cc889b53559dc23fe755da5a333a67b729da3943
CarltonNcube <carltonncube@outlook.com>
2024-01-08 09:41:29 -0800

    endpoint (route)

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,6 +13,7 @@ from models.review import Review
 from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+
 name2class = {
     'Amenity': Amenity,
     'City': City,
@@ -35,58 +36,21 @@ class DBStorage:
         host = os.getenv('HBNB_MYSQL_HOST')
         database = os.getenv('HBNB_MYSQL_DB')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, passwd, host, database))
+                                      .format(user, passwd, host, database),
+                                      pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-<<<<<<< HEAD
-        """returns a dictionary of all the objects present"""
-        if not self.__session:
-            self.reload()
-        objects = {}
-        if type(cls) == str:
-            cls = name2class.get(cls, None)
-        if cls:
-            for obj in self.__session.query(cls):
-                objects[obj.__class__.__name__ + '.' + obj.id] = obj
-        else:
-            for cls in name2class.values():
-                for obj in self.__session.query(cls):
-                    objects[obj.__class__.__name__ + '.' + obj.id] = obj
-        return objects
-
-    def reload(self):
-        """reloads objects from the database"""
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(session_factory)
-=======
         """query on the current database session"""
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
+        for clss in name2class.values():
+            if cls is None or cls is clss:
+                objs = self.__session.query(clss).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
-        return (new_dict)
- 
-    def get(self, cls, id):
-        """Retrieve one object"""
-        return self.__session.query(cls).filter_by(id=id).first()
-
-    def count(self, cls=None):
-        """Count the number of objects in storage"""
-        if cls:
-            return self.__session.query(cls).count()
-        else:
-            count = 0
-            for clss in classes.values():
-                count += self.__session.query(clss).count()
-            return count
->>>>>>> 36505c1647ff288e6c8ad4385e1e78c06ff9b35e
+        return new_dict
 
     def new(self, obj):
         """creates a new object"""
@@ -98,14 +62,19 @@ class DBStorage:
 
     def delete(self, obj=None):
         """deletes an object"""
-        if not self.__session:
-            self.reload()
         if obj:
             self.__session.delete(obj)
 
     def close(self):
-        """Dispose of current session if active"""
+        """Dispose of the current session if active"""
         self.__session.remove()
+
+    def reload(self):
+        """reloads objects from the database"""
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(session_factory)
 
     def get(self, cls, id):
         """A method to retrieve one object"""
@@ -116,10 +85,16 @@ class DBStorage:
             for item, value in objs.items():
                 if value.id == id:
                     return value
-        
+
     def count(self, cls=None):
         """A method to count the number of objects in storage"""
         new_cnt = self.all(cls)
         if cls in name2class.values():
             new_cnt = self.all(cls)
         return len(new_cnt)
+
+
+# Instantiate the DBStorage class
+storage = DBStorage()
+# Reload the storage session
+storage.reload()
